ef62420544faedf8e30057999802dc762ecaf3e0
Guido U. Draheim <guidod@gmx.de>
2009-10-21 22:31:08 +0000

    FCode to FXCode

--- testmodule2/src/main/c/testmodule2-ext.c
+++ testmodule2/src/main/c/testmodule2-ext.c
@@ -9,22 +9,22 @@
 
 #include <pfe/pfe-base.h>
 
-/* The macro FCode creates the callframe info needed by C to be a forth prim */
+/* The macro void FXCode creates the callframe info needed by C to be a forth prim */
 static
-FCode (p4_hello)
+void FXCode (p4_hello)
 {
     p4_outs ("\nHello World!\n");
 }
 
-/* the pfe export table binds FCode primitives (in C) to their forth name.
+/* the pfe export table binds FXCode primitives (in C) to their forth name.
  * it carries other information as well interpreted at load-time of the module.
  */
-P4_LISTWORDS(testmodule2) =
+P4_LISTWORDSET (testmodule2) [] =
 {
     P4_INTO ("EXTENSIONS", 0),
     P4_FXco ("hello", p4_hello),
 };
-P4_COUNTWORDS(testmodule2, "TESTMODULE2 - example module implementation");
+P4_COUNTWORDSET (testmodule2, "TESTMODULE2 - example module implementation");
 
 /* emacs...
  * Local variables:
